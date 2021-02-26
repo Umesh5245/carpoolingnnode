78a9d69222ab46df1af03cc894466db4b2d9a0a3
Umesh5245 <umesh.agarwal102675@marwadiuniversity.ac.in>
2021-02-26 13:29:33 +0530

    4

---
+++ 4/node/controllers/UserController.js
@@ -0,0 +1,106 @@
+const User =require('../models/User')
+
+const index=(req,res,next)=>{
+    User.find().then(response=>{
+        res.json({
+            response
+        })
+    })
+    .catch(error=>{
+        res.json({
+            message:'an eror occored'
+        })
+    })
+}
+const show =(req,res,next)=>{
+    let phone= req.body.phone
+
+    let name= req.body.name
+    let email= req.body.email
+    let college_id_number= req.body.college_id_number
+    let profile_pic= req.body.profile_pic
+    let gender= req.body.gender
+    let category= req.body.category
+    let department= req.body.department
+
+    User.find({phone:{ $regex: new RegExp(phone, "ig")   },name:{ $regex: new RegExp(name, "ig")},email:{ $regex: new RegExp(email, "ig")},college_id_number:{ $regex: new RegExp(college_id_number, "ig")},profile_pic:{ $regex: new RegExp(profile_pic, "ig")},gender:{ $regex: new RegExp(gender, "ig")},category:{ $regex: new RegExp(category, "ig")},department:{ $regex: new RegExp(department, "ig")}})
+    .then(response =>{
+        res.json({
+            response,phone
+        })
+    })
+    .catch(error=>{
+        res.json({
+            message:`am error occuired ${error}`
+        })
+        
+    })
+}
+
+const store =(req,res,next)=>{
+    let user= new User({
+        name: req.body.name,
+        email:req.body.email,
+        phone:req.body.phone,
+        college_id_number:req.body.college_id_number,
+        profile_pic:req.body.profile_pic,
+        gender:req.body.gender,
+        category:req.body.category,
+        department:req.body.department
+    })
+    
+
+    user.save()
+    .then(response =>{
+        res.json({
+            message:"employye added"
+        })
+    })
+    .catch(error=>{
+        res.json({
+            message:`am error occuired ${error}`
+        })
+        
+    })
+}
+
+const update =(req,res,next)=>{
+    let name=req.body.name
+    let updateData=  {
+
+        userID: req.body.userID,
+        dept:req.body.dept,
+        phone:req.body.phone,
+        college_id:req.body.college_id,
+        category:req.body.category
+    }
+User.update(name,{ name: 'jason bourne' })    .then(response =>{
+        res.json({
+            message:"employye update"
+        })
+    })
+    .catch(error=>{
+        res.json({
+            message:`am error occuired ${userID} `
+        })
+        
+    })
+}
+
+
+const destroy =(req,res,next)=>{
+    let userID=req.body.userID
+   
+User.findOneAndDelete(userID)    .then(response =>{
+        res.json({
+            message:"employye deletd"
+        })
+    })
+    .catch(error=>{
+        res.json({
+            message:`am error occuired`
+        })
+        
+    })
+}
+module.exports={index,show,store,update,destroy}      
---
+++ 4/node/models/User.js
@@ -0,0 +1,35 @@
+const  mongoose = require('mongoose')
+
+const schema =  mongoose.Schema
+const userSchema =new mongoose.Schema({
+phone:{
+    type: String,
+    unique: true,
+    index: true,
+    required: true,
+},
+    name:{
+        type: String
+    },
+    email:{
+        type: String
+    },
+ 
+    college_id_number:{
+        type:String
+    },
+    profile_pic:{
+        type: String
+    },
+    gender:{
+        type: String
+    },
+    category:{
+        type: String
+    },department:{
+        type: String
+    }
+},  { collection: 'user_details' })
+userSchema.set('timestamps', true); 
+const User=mongoose.model('User',userSchema)
+module.exports= User
